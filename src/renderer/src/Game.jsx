e6b953fea912dcb5a16d940f4c3eed34e8fbfb85
Draegonis <ddmDraegonis@hotmail.com>
2023-10-14 14:46:14 -0400

    move save/load indicators to own folder

--- src/renderer/src/Game.jsx
+++ src/renderer/src/Game.jsx
@@ -1,7 +1,7 @@
 import { GameCommands } from './components/inputs/GameCommands'
 import { GameFunctions } from './components/funcs/GameFunctions'
-import { SaveIndicator } from './components/SaveIndicator'
-import { LoadIndicator } from './components/LoadIndicator'
+import { SaveIndicator } from './components/ui/indicators/SaveIndicator'
+import { LoadIndicator } from './components/ui/indicators/LoadIndicator'
 
 const Game = () => {
   return (
