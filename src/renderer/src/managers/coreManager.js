75b1da831e1da242421cbf94e2a157fba5583dba
Draegonis <ddmDraegonis@hotmail.com>
2023-10-24 16:06:47 -0400

    add jsDocs to core manager.

--- src/renderer/src/managers/coreManager.js
+++ src/renderer/src/managers/coreManager.js
@@ -1,9 +1,20 @@
 export const fileExtension = 'rmmzsave'
 
 class Ddm_CoreManager {
-  #dataPromises = [] // promise array to pass into Promise.all
+  /**
+   * holds the task data to pass into Promise.all.
+   */
+  #dataPromises = []
+
+  /**
+   * Property that determines if when saving/loading that all files should be grouped into the same folder.
+   */
   saveAsFolder = true
 
+  /**
+   * This is called before the Promise.all on an array of tasks.
+   * @param {string} mode a string containing the task type. Ex: save
+   */
   #startTask(mode) {
     if (!window.$APP) return
     switch (mode) {
@@ -13,6 +24,10 @@ class Ddm_CoreManager {
     }
   }
 
+  /**
+   * This is called after the Promise.all on an array of tasks.
+   * @param {string} mode a string containing the task type. Ex: save
+   */
   #endTask(mode) {
     if (!window.$APP) return
     switch (mode) {
@@ -22,18 +37,50 @@ class Ddm_CoreManager {
     }
   }
 
+  /**
+   * Sends an async request to the electron side in order to determine if a file exists or not.
+   * @param {string} folder the folder to check for the file inside it.
+   * @param {string} file the filename to check with the extension.
+   * @returns {boolean} true if the file exists or false if it fails or the file doesn't exists.
+   */
   async fileExists(folder, file) {
     return await window.electron.ipcRenderer.invoke('file-exists', folder, file)
   }
 
+  /**
+   * Sends an async request to electron to save an object. The object cannot contain any functions
+   * or electron will throw an error. The compress variable will turn the object into a buffer
+   * when saving.
+   * @param {string} folder the folder in which to save data.
+   * @param {string} file the filename in which to save as.
+   * @param {object} data the data object to save.
+   * @param {boolean} compress whether or not to compress the data into a buffer when saving the file.
+   * @returns {boolean} true if the saving succeeded, false if it fails.
+   */
   async saveToFile(folder, file, data, compress) {
     return await window.electron.ipcRenderer.invoke('save-object', folder, file, data, compress)
   }
 
+  /**
+   * Sends an async request to electron to read a file and retrieve the data that was saved. deCompressed
+   * variable lets electron know to restore the data into a json state before sending it back.
+   * @param {*} folder the folder in where the file resides.
+   * @param {*} file the filename with extension to be read.
+   * @param {*} deCompress whether the data inside the file was compressed and needs to be decompressed.
+   * @returns {object | boolean}
+   */
   async loadFromFile(folder, file, deCompress) {
     return await window.electron.ipcRenderer.invoke('read-object', folder, file, deCompress)
   }
 
+  /**
+   * From the given task data this function creates a Promise to be consumed in a Promise.all array of tasks.
+   * @param {string} savefileId used in concatenating the saveName.
+   * @param {string} extension used in concatenating the saveName.
+   * @param {string} task the type of data task being performed.
+   * @param {object | undefined} contents an object with contents to be saved or undefined.
+   * @returns {Promise} a promise to be run within a Promise.all.
+   */
   #makeDataTask(savefileId, extension, task, contents) {
     const saveName = savefileId + '.' + extension
     const saveFolder = CoreManager.saveAsFolder ? `save/${savefileId}` : 'save'
@@ -64,10 +111,20 @@ class Ddm_CoreManager {
     })
   }
 
+  /**
+   * A function meant to be used externally in order to register tasks for the Promise.all array.
+   * @param {{ savefileId: string, extension: string, task: 'save' | 'load', contents: object | undefined, thenCallback: function, catchCallback: function }} taskContents
+   * an object with the properties needed in order to create data tasks.
+   */
   registerDataTask(taskContents) {
     this.#dataPromises.push(taskContents)
   }
 
+  /**
+   * Function called externally in order to run all the registered tasks. Used to handle multiple files when saving and loading.
+   * @param {'save' | 'load'} mode the key passed into startTask and endTask in order to run additional code.
+   * @returns {boolean} true if all the promises succeed, or false as long as one fails.
+   */
   async executeDataTasks(mode) {
     this.#startTask(mode)
 
