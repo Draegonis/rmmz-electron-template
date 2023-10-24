7f63ff5b92d8e2a0227bfae497ec8a4780b7abdf
Draegonis <ddmDraegonis@hotmail.com>
2023-10-24 16:58:31 -0400

    add comments to gameParsers.js, small fix.

--- src/renderer/src/helpers/gameParsers.js
+++ src/renderer/src/helpers/gameParsers.js
@@ -1,8 +1,28 @@
+/**
+ * A custom parser to take in a JSON parsable self switch id and turn it into a usable key for self switches.
+ * @param {string} id a JSON parsable string which contains: '{mapId, eventId, switchId}'
+ * @returns {[number, number, string]}
+ */
 export const parseSelfSW = (id) => {
   const newId = JSON.parse(id)
-  return [Number(newId.mapId || -1), Number(newId.eventId || -1), newId.switchId.toUpperCase()]
+  return [
+    parseNumber(newId.mapId, -1),
+    parseNumber(newId.eventId, -1),
+    newId.switchId.toUpperCase()
+  ]
 }
 
+/**
+ * A simple parser to convert a string value into a usable number.
+ * @param {string} value the value to attempt to convert into a number.
+ * @param {number} min the failsafe number if it fails to convert the value.
+ * @returns {number}
+ */
 export const parseNumber = (value, min) => Number(value || min ? min : -1)
 
+/**
+ * A simple parser to convert a string value into a usable boolean.
+ * @param {string} value the value to check if it is 'true'.
+ * @returns {boolean}
+ */
 export const parseBoolean = (value) => value.toLowerCase() === 'true'
