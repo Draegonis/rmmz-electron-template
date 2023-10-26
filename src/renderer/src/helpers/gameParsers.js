a357b8ff848cce91f5069a1d41d67eb49c638eff
Draegonis <ddmDraegonis@hotmail.com>
2023-10-25 23:07:03 -0400

    fix parse number

--- src/renderer/src/helpers/gameParsers.js
+++ src/renderer/src/helpers/gameParsers.js
@@ -3,6 +3,10 @@ export const parseSelfSW = (id) => {
   return [Number(newId.mapId || -1), Number(newId.eventId || -1), newId.switchId.toUpperCase()]
 }
 
-export const parseNumber = (value, min) => Number(value || min ? min : -1)
-
+export const parseNumber = (value, min) => {
+  let num = undefined
+  num = Number(value)
+  if (!num) num = min ? min : -1
+  return num
+}
 export const parseBoolean = (value) => value.toLowerCase() === 'true'
