63e0215328ede435b95bce244fa3f12826cc2955
Draegonis <ddmDraegonis@hotmail.com>
2023-10-25 23:07:42 -0400

    add jsdocs to gameParsers

--- src/renderer/src/helpers/gameParsers.js
+++ src/renderer/src/helpers/gameParsers.js
@@ -1,3 +1,8 @@
+/**
+ * A custom parser to take in a JSON parsable self switch id and turn it into a usable key for self switches.
+ * @param {string} id a JSON parsable string which contains: '{mapId, eventId, switchId}'
+ * @returns {[number, number, string]}
+ */
 export const parseSelfSW = (id) => {
   const newId = JSON.parse(id)
   return [
@@ -7,10 +12,22 @@ export const parseSelfSW = (id) => {
   ]
 }
 
+/**
+ * A simple parser to convert a string value into a usable number.
+ * @param {string} value the value to attempt to convert into a number.
+ * @param {number} min the failsafe number if it fails to convert the value.
+ * @returns {number}
+ */
 export const parseNumber = (value, min) => {
   let num = undefined
   num = Number(value)
   if (!num) num = min ? min : -1
   return num
 }
+
+/**
+ * A simple parser to convert a string value into a usable boolean.
+ * @param {string} value the value to check if it is 'true'.
+ * @returns {boolean}
+ */
 export const parseBoolean = (value) => value.toLowerCase() === 'true'
